fa5427da9c6a821c073e913d950c7fb6d8a6a8c1
Angie <angie.peck@gmail.com>
2020-05-28 16:09:52 -0500

    functional sidebar with no info

--- logic.js
+++ logic.js
@@ -5,7 +5,10 @@ d3.csv("cleaned_restaurant_data.csv").then(function(csv) {
 
 
 // define map variable
-var map = L.map('map');
+var map = L.map('map', {
+  center: [37.96, -91.83],
+  zoom: 7
+});
 // define map tile layer and add to the map
 var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -14,10 +17,23 @@ var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
     accessToken: API_KEY
   });
 streetmap.addTo(map);
-var sidebar = L.control.sidebar({ sidebar }).addTo(map);
 
-// set centerpoint and zoom of map
-map.setView([37.96, -91.83], 7);
+// create sidebar
+var sidebar = L.control.sidebar({
+  autopan: false,
+  closeButton: true,
+  container: 'sidebar',
+  position: 'right'
+}).addTo(map);
+
+// add content to sidebar
+var panelContent = {
+  id: 'userinfo',
+  tab: '<i class="fa-gear"></i>',
+  title: 'Poverty Data',
+  Position: 'bottom'
+};
+sidebar.addPanel(panelContent);
 
 // add restaurant markers to map  
 var data;
@@ -26,7 +42,10 @@ function addRestaurantMarkers() {
     var restaurantMarker = L.marker([+d.latitude, +d.longitude]);
     restaurantMarker.bindPopup("<h3>" + d.name + "</h3><h4>" + d.loopcheck  + "</h4>");
     restaurantMarker.addTo(map);
+    //updateSidebarData();
   })
 }
+
+
 
 
